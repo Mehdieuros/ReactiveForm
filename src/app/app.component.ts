3d7e0e239a49c0abb80af44aaa273e40af09796d
Mehdieuros <nwo92@hotmail.fr>
2017-05-12 15:50:04 +0200

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  rForm: FormGroup;
+  post:any;                     // A property for our submitted form
+  description:string = '';
+  titleAlert:string = 'This field is required';
+  name:string = '';
+
+
+constructor(private fb: FormBuilder) { 
+
+    this.rForm = fb.group({
+      'name' : [null, Validators.required],
+      'description' : [null, Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)])],
+      'validate' : ''
+    });
+
+  }
+  addPost(post) {
+    this.description = post.description;
+    this.name = post.name;
+  }
+  ngOnInit() {
+    this.rForm.get('validate').valueChanges.subscribe(
+
+      (validate) => {
+
+          if (validate == '1') {
+              this.rForm.get('name').setValidators([Validators.required, Validators.minLength(3)]);
+              this.titleAlert = 'You need to specify at least 3 characters';
+          } else {
+              this.rForm.get('name').setValidators(Validators.required);
+          }
+          this.rForm.get('name').updateValueAndValidity();
+
+      });
+  }
+}
